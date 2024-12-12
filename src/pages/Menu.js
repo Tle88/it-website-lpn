dc186784a20a50fa3cb03e7278d9eec843aa54fa
Tle88 <wongsakorn781@gmail.com>
2024-12-12 10:43:12 +0700

    Tle commit

---
+++ src/pages/Menu.js
@@ -0,0 +1,73 @@
+import React from "react";
+
+function Menu (){
+    return (
+        <section class="container my-5">
+            <h2 class="text-center mb-4">Our Menu</h2>
+            <div class="row">
+                
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <img src="https://static.thairath.co.th/media/B6FtNKtgSqRqbnNsbKFRA9Hw1ddaiN8vczDH5awGUi4JQ7XTjwF2YTlnGfAZTGUAcQDXv.jpg"></img>
+                        <div class="card-body">
+                            <h5 class="card-title">ต้มยำกุ้ง</h5>
+                            <p class="card-text">รายละเอียด</p>
+                            <p class="text-muted">ราคา: 50</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <img src="https://static.thairath.co.th/media/4DQpjUtzLUwmJZZSDsxtz2w72aIAyf9iOjncow0cvYfP.jpg"></img>
+                        <div class="card-body">
+                            <h5 class="card-title">กระเพราหมูสับ</h5>
+                            <p class="card-text">รายละเอียด</p>
+                            <p class="text-muted">ราคา: 50</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <img src="https://static.thairath.co.th/media/00_A5B2824678C1E1280.webp"></img>
+                        <div class="card-body">
+                            <h5 class="card-title">ปลาราดพริก</h5>
+                            <p class="card-text">รายละเอียด</p>
+                            <p class="text-muted">ราคา: 60</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <img src="https://static.thairath.co.th/media/00_A5B2657966C2E480.webp"></img>
+                        <div class="card-body">
+                            <h5 class="card-title">แกงเขียวหวาน</h5>
+                            <p class="card-text">รายละเอียด</p>
+                            <p class="text-muted">ราคา: 45</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <img src="https://static.thairath.co.th/media/00_A5B2317805C2E480.webp"></img>
+                        <div class="card-body">
+                            <h5 class="card-title">ไก่แซ่บ</h5>
+                            <p class="card-text">รายละเอียด</p>
+                            <p class="text-muted">ราคา: 35</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <img src="https://static.thairath.co.th/media/00_A5B2420071C1E700.webp"></img>
+                        <div class="card-body">
+                            <h5 class="card-title">หมูกรอบผัดพริกเกลือ</h5>
+                            <p class="card-text">รายละเอียด</p>
+                            <p class="text-muted">ราคา: 50</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+    export default Menu;
